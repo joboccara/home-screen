80126573277ecb74d35bae98ef8688e47eb15567
Jonathan Boccara <boccaraj@gmail.com>
2024-10-28 14:29:11 +0100

    Add platform specific code

--- src/font_location.py
+++ src/font_location.py
@@ -1,1 +1,9 @@
-FONT_LOCATION = '/System/Library/Fonts/Helvetica.ttc'
+import platform
+system = platform.system()
+
+print(system)
+
+if system == 'Darwin':
+    FONT_LOCATION = '/System/Library/Fonts/Helvetica.ttc'
+else:
+    FONT_LOCATION = '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf'
