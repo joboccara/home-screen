8b0629f8622c39f3bbdbc23e9e15736d009c08f3
Jonathan Boccara <boccaraj@gmail.com>
2024-10-28 10:58:09 +0100

    Scrape start time

--- src/rain-hour-scraping.py
+++ src/rain-hour-scraping.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,24 +7,37 @@ from selenium.webdriver.support import expected_conditions as EC
 url = "https://meteofrance.com/previsions-meteo-france/neuilly-sur-seine/92200"
 ul_rain_data_class_name = 'rain-data'
 number_of_rain_data_points = 9
+div_class_name_containing_time = 'time'
+start_time_format =  "%H : %M"
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(options=options)
+def scrape_rain_hour():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
 
-try:
-    driver.get(url)
+    result = { "rain_labels": [] }
 
-    wait = WebDriverWait(driver, 10)
-    ul_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, ul_rain_data_class_name)))
-    wait.until(lambda d: len(d.find_elements(By.XPATH, f'//ul[@class="{ul_rain_data_class_name}"]/li')) == number_of_rain_data_points)
+    try:
+        driver.get(url)
 
-    li_elements = ul_element.find_elements(By.TAG_NAME, "li")
-    for li in li_elements:
-        img = li.find_element(By.TAG_NAME, "img")
-        if img and img.get_attribute("alt"):
-            print(img.get_attribute("alt"))
-finally:
-    driver.quit()
+        wait = WebDriverWait(driver, 10)
+        ul_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, ul_rain_data_class_name)))
+        wait.until(lambda d: len(d.find_elements(By.XPATH, f'//ul[@class="{ul_rain_data_class_name}"]/li')) == number_of_rain_data_points)
+
+        li_elements = ul_element.find_elements(By.TAG_NAME, "li")
+        for li in li_elements:
+            img = li.find_element(By.TAG_NAME, "img")
+            if img and img.get_attribute("alt"):
+                result["rain_labels"].append(img.get_attribute("alt"))
+
+        div_time = driver.find_element(By.CLASS_NAME, div_class_name_containing_time)
+        start_time_s = div_time.find_element(By.TAG_NAME, "p").text
+        result["start_time"] = datetime.strptime(start_time_s, start_time_format)
+    finally:
+        driver.quit()
+        return result
+
+
+print(scrape_rain_hour())
