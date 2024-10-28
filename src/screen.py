e6bad13716c8c0813cbda44a7f951640278e2a81
Jonathan Boccara <boccaraj@gmail.com>
2024-10-28 14:32:41 +0100

    Screen image in function

--- src/screen.py
+++ src/screen.py
@@ -2,8 +2,7 @@ from date_time_widget import DateTimeWidget
 from pen import Pen
 from PIL import Image
 
-image = Image.new('1', (480, 800), 255)
-
-DateTimeWidget().draw(Pen(image, (100, 50)))
-
-image.show()
+def generate_screen_image():
+    image = Image.new('1', (480, 800), 255)
+    DateTimeWidget().draw(Pen(image, (100, 50)))
+    return image
