647795f6a08ec88d14e426f0c0fd90e1bd2e418e
Jonathan Boccara <boccaraj@gmail.com>
2024-10-30 23:10:21 +0100

    Fix size and positions

--- src/rain_hour_widget.py
+++ src/rain_hour_widget.py
@@ -6,6 +6,6 @@ class RainHourWidget:
         light_rain_path = os.path.join(current_path, "images/light-rain.png")
         pen.draw_picture((0, 0), light_rain_path, 25, 25)
         medium_rain_path = os.path.join(current_path, "images/medium-rain.png")
-        pen.draw_picture((0, 0), medium_rain_path, 60, 25)
+        pen.draw_picture((30, 0), medium_rain_path, 25, 25)
         heavy_rain_path = os.path.join(current_path, "images/heavy-rain.png")
-        pen.draw_picture((0, 0), heavy_rain_path, 100, 25)
+        pen.draw_picture((60, 0), heavy_rain_path, 25, 25)
