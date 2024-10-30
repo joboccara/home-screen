1cb8bd52a966d7417bc2188ff9a57fa5f7d63213
Jonathan Boccara <boccaraj@gmail.com>
2024-10-30 23:09:38 +0100

    Medium and heavy rain images

--- src/rain_hour_widget.py
+++ src/rain_hour_widget.py
@@ -5,3 +5,7 @@ class RainHourWidget:
         current_path = os.path.dirname(os.path.abspath(__file__))
         light_rain_path = os.path.join(current_path, "images/light-rain.png")
         pen.draw_picture((0, 0), light_rain_path, 25, 25)
+        medium_rain_path = os.path.join(current_path, "images/medium-rain.png")
+        pen.draw_picture((0, 0), medium_rain_path, 60, 25)
+        heavy_rain_path = os.path.join(current_path, "images/heavy-rain.png")
+        pen.draw_picture((0, 0), heavy_rain_path, 100, 25)
