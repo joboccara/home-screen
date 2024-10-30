3e7c0879c2bff9cf8c54393e6988298487cc2a2a
Jonathan Boccara <boccaraj@gmail.com>
2024-10-30 13:29:41 +0100

    Mutualize code between datetime draw and width

--- src/date_time_widget.py
+++ src/date_time_widget.py
@@ -3,28 +3,24 @@ from datetime import datetime
 from font_utils import FONT_LOCATION, textsize
 
 class DateTimeWidget:
-    def draw(self, pen):
-        time_string = datetime.now().strftime("%H:%M")
-        time_font = ImageFont.truetype(FONT_LOCATION, 60)
-        time_x, time_y = 0, 0
-        pen.text((time_x, time_y), time_string, time_font)
+    def __init__(self):
+        self.time_string = datetime.now().strftime("%H:%M")
+        self.time_font = ImageFont.truetype(FONT_LOCATION, 60)
+        self.time_width, self.time_height = textsize(self.time_string, self.time_font)
 
-        date_font = ImageFont.truetype(FONT_LOCATION, 20)
-        date_string = datetime.now().strftime("%B, %d")
-        time_width, time_height = textsize(time_string, time_font)
-        date_width, _ = textsize(date_string, date_font)
-        date_x = time_x + time_width / 2 - date_width / 2
+        self.date_string = datetime.now().strftime("%B, %d")
+        self.date_font = ImageFont.truetype(FONT_LOCATION, 20)
+        self.date_width, _ = textsize(self.date_string, self.date_font)
+
+    def draw(self, pen):
+        time_x, time_y = 0, 0
+        pen.text((time_x, time_y), self.time_string, self.time_font)
+
+        date_x = time_x + self.time_width / 2 - self.date_width / 2
         spacing = 10
-        date_y = time_height + spacing
-        pen.text((date_x, date_y), date_string, date_font)
+        date_y = self.time_height + spacing
+        pen.text((date_x, date_y), self.date_string, self.date_font)
 
     def width(self):
-        time_string = datetime.now().strftime("%H:%M")
-        time_font = ImageFont.truetype(FONT_LOCATION, 60)
-        time_width, _ = textsize(time_string, time_font)
+        return max(self.time_width, self.date_width)
 
-        date_font = ImageFont.truetype(FONT_LOCATION, 20)
-        date_string = datetime.now().strftime("%B, %d")
-        date_width, _ = textsize(date_string, date_font)
-
-        return max(time_width, date_width)
