e9ee51ec073d4bf3d8ed13cc3c339e38dc072c80
Jonathan Boccara <boccaraj@gmail.com>
2024-10-30 14:42:38 +0100

    Extract wrapping code into Pen

--- src/pen.py
+++ src/pen.py
@@ -1,4 +1,5 @@
 from PIL import ImageDraw
+from font_utils import textwidth, textheight
 
 class Pen:
     def __init__(self, image, offset) -> None:
@@ -7,3 +8,27 @@ class Pen:
 
     def write(self, offset, content, font) -> None:
         self.draw.text((self.offset[0] + offset[0], self.offset[1] + offset[1]), content, font=font, fill=0)
+
+    def write_wrapped(self, pen, offset, content, font, max_width):
+        line_spacing = font.size / 3
+        for index, line in enumerate(wrap_text_to_lines(content, font, max_width)):
+            line_height = textheight(line, font) + line_spacing
+            line_y = index * line_height
+            pen.write((offset[0], line_y + offset[1]), line, font)
+
+
+def wrap_text_to_lines(text, font, max_width):
+    words = text.split()
+    lines = []
+    current_line = ""
+    for word in words:
+        current_line_with_word = f"{current_line} {word}".strip()
+        this_seems_to_need_an_adjustment_i_dont_know_why = 20
+        if textwidth(current_line_with_word, font) <= max_width - this_seems_to_need_an_adjustment_i_dont_know_why:
+            current_line = current_line_with_word
+        else:
+            lines.append(current_line)
+            current_line = word
+    if current_line != "":
+        lines.append(current_line)
+    return lines
--- src/today_in_history_widget.py
+++ src/today_in_history_widget.py
@@ -2,29 +2,6 @@ from PIL import ImageFont
 from datetime import datetime
 from font_utils import FONT_LOCATION, textsize, textwidth, textheight
 
-def wrap_text_to_lines(text, font, max_width):
-    words = text.split()
-    lines = []
-    current_line = ""
-    for word in words:
-        current_line_with_word = f"{current_line} {word}".strip()
-        this_seems_to_need_an_adjustment_i_dont_know_why = 20
-        if textwidth(current_line_with_word, font) <= max_width - this_seems_to_need_an_adjustment_i_dont_know_why:
-            current_line = current_line_with_word
-        else:
-            lines.append(current_line)
-            current_line = word
-    if current_line != "":
-        lines.append(current_line)
-    return lines
-
-def write_wrapped(pen, offset, text, font, max_width):
-    line_spacing = font.size / 3
-    for index, line in enumerate(wrap_text_to_lines(text, font, max_width)):
-        line_height = textheight(line, font) + line_spacing
-        line_y = index * line_height
-        pen.write((offset[0], line_y + offset[1]), line, font)
-
 class TodayInHistoryWidget:
     def draw(self, pen, width):
         year = 1440
@@ -40,8 +17,8 @@ class TodayInHistoryWidget:
         content_x = 0
         content_y = title_y + title_height + spacing
         content_font = ImageFont.truetype(FONT_LOCATION, 15)
-        write_wrapped(pen,
-                      (content_x, content_y),
-                      "Emperor Gratian elevates Flavius Theodosius at Sirmium to Augustus, and gives him authority over all the eastern provinces of the Roman Empire.",
-                      content_font,
-                      width)
+        pen.write_wrapped(pen,
+                          (content_x, content_y),
+                          "Emperor Gratian elevates Flavius Theodosius at Sirmium to Augustus, and gives him authority over all the eastern provinces of the Roman Empire.",
+                          content_font,
+                          width)
