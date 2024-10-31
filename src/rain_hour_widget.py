6fef8e781566a70903203a948f8e1fce5b56d528
Jonathan Boccara <boccaraj@gmail.com>
2024-10-31 14:23:01 +0100

    Add title and fix rain in the hour

--- src/rain_hour_widget.py
+++ src/rain_hour_widget.py
@@ -6,38 +6,50 @@ from font_utils import FONT_LOCATION, text_height, text_width
 class RainHourWidget:
     ICON_SIZE = 25
     ICON_SPACING = 5
+    TITLE_SPACING = 20
     DASH = "-"
     NO_RAIN_LABEL = "No rain in the hour"
-    NO_RAIN_FONT = ImageFont.truetype(FONT_LOCATION, 15)
+    TITLE = "Rain in the hour:"
+    TITLE_FONT = ImageFont.truetype(FONT_LOCATION, 15)
 
     def __init__(self, api_access, weather_page_driver):
         self.rain_intensity_by_datetime = api_access.get_rain_intensity_by_datetime(weather_page_driver)
 
     def draw(self, pen):
         if self.rains_in_the_hour():
-            no_rain_x = self.width() / 2 - text_width(self.NO_RAIN_LABEL, self.NO_RAIN_FONT) / 2
-            pen.write((no_rain_x, 0), self.NO_RAIN_LABEL, self.NO_RAIN_FONT)
+            title_height = text_height(self.TITLE, self.TITLE_FONT)
+            title_x = 0
+            title_y = self.height() / 2 - title_height / 2
+            pen.write((title_x, title_y), self.TITLE, self.TITLE_FONT)
+            offset_x = text_width(self.TITLE, self.TITLE_FONT) + self.TITLE_SPACING
+            self._draw_rain_images(pen, self.rain_intensity_by_datetime, offset_x)
+            self._draw_time_labels(pen, self.rain_intensity_by_datetime, offset_x, offset_y=self.ICON_SIZE + self.ICON_SPACING)
         else:
-            self._draw_rain_images(pen, self.rain_intensity_by_datetime)
-            self._draw_time_labels(pen, self.rain_intensity_by_datetime, offset_y=self.ICON_SIZE + self.ICON_SPACING)
+            no_rain_x = self.width() / 2 - text_width(self.NO_RAIN_LABEL, self.TITLE_FONT) / 2
+            pen.write((no_rain_x, 0), self.NO_RAIN_LABEL, self.TITLE_FONT)
 
     def width(self):
-        return len(self.rain_intensity_by_datetime.items()) * (self.ICON_SIZE + self.ICON_SPACING) - self.ICON_SPACING
+        if self.rains_in_the_hour():
+            title_width = text_width(self.TITLE, self.TITLE_FONT)
+            rain_images_width = len(self.rain_intensity_by_datetime.items()) * (self.ICON_SIZE + self.ICON_SPACING) - self.ICON_SPACING
+            return title_width + self.TITLE_SPACING + rain_images_width
+        else:
+            return text_width(self.NO_RAIN_LABEL, self.TITLE_FONT)
 
     def height(self):
         if self.rains_in_the_hour():
-            return text_height(self.NO_RAIN_LABEL, self.NO_RAIN_FONT)
-        else:
             return self.ICON_SIZE
+        else:
+            return text_height(self.NO_RAIN_LABEL, self.TITLE_FONT)
 
     def rains_in_the_hour(self):
-        return all(value == 0 for value in self.rain_intensity_by_datetime.values())
+        return any(value != 0 for value in self.rain_intensity_by_datetime.values())
 
-    def _draw_rain_images(self, pen, rain_intensity_by_datetime):
+    def _draw_rain_images(self, pen, rain_intensity_by_datetime, offset_x):
         light_rain, medium_rain, heavy_rain = self._rain_images()
         dash_font = ImageFont.truetype(FONT_LOCATION, 15)
         for index, rain_intensity in enumerate(rain_intensity_by_datetime.values()):
-            x = (self.ICON_SIZE + self.ICON_SPACING) * index
+            x = offset_x + (self.ICON_SIZE + self.ICON_SPACING) * index
             y = 0
             if rain_intensity == 0:
                 dash_x = x + self.ICON_SIZE / 2 - text_width(self.DASH, dash_font) / 2
@@ -50,11 +62,11 @@ class RainHourWidget:
             elif rain_intensity == 3:
                 pen.draw_picture((x, y), heavy_rain)
 
-    def _draw_time_labels(self, pen, rain_intensity_by_datetime, offset_y):
+    def _draw_time_labels(self, pen, rain_intensity_by_datetime, offset_x, offset_y):
         minutes_font = ImageFont.truetype(FONT_LOCATION, 10)
         for index, datetime in enumerate(rain_intensity_by_datetime.keys()):
             minute_label = datetime.strftime("%H:%M" if index == 0 else "%M")
-            x = self.minute_label_x(index, minute_label, minutes_font)
+            x = offset_x + self.minute_label_x(index, minute_label, minutes_font)
             y = offset_y
             pen.write((x, y), minute_label, minutes_font)
 
