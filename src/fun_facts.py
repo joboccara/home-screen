3fbd413b66364e71a71b1da099915d9c3ae10376
Jonathan Boccara <boccaraj@gmail.com>
2024-11-03 20:57:19 +0100

    Mutualize code to get facts

--- src/fun_facts.py
+++ src/fun_facts.py
@@ -4,18 +4,20 @@ import os
 import random
 import requests
 
+API_KEY = os.getenv("API_NINJAS_API_KEY")
+
 def get_fun_facts():
-    api_key = os.getenv("API_NINJAS_API_KEY")
     today_in_history_url = f"https://api.api-ninjas.com/v1/historicalevents?day={datetime.now().day}&month={datetime.now().month}"
-    response = json.loads(requests.get(today_in_history_url, headers={'X-Api-Key': api_key}).text)
+    response = _fetch(today_in_history_url)
     today_in_history = response[random.randint(0, len(response) - 1)] if len(response) > 0 else None
 
     chuck_norris_url = "https://api.api-ninjas.com/v1/chucknorris"
-    response = json.loads(requests.get(chuck_norris_url, headers={'X-Api-Key': api_key}).text)
-    chuck_norris = response["joke"]
+    chuck_norris = _fetch(chuck_norris_url)["joke"]
 
     return {
         "today_in_history": today_in_history,
         "chuck_norris": chuck_norris
         }
 
+def _fetch(url):
+    return json.loads(requests.get(url, headers={'X-Api-Key': API_KEY}).text)
