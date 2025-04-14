99cae1db63e5efc9543311768a6cae842c355abe
Jonathan Boccara <boccaraj@gmail.com>
2025-04-14 23:30:47 +0200

    Handle missing information in zmanim

--- src/zmanim_scraping.py
+++ src/zmanim_scraping.py
@@ -7,9 +7,15 @@ def scrape_zmanim(calj_driver):
         tzet_time = _find_time_from_label(calj_driver, "tzeit hacokhavim")
 
         sh_start_label_span = calj_driver.find_element(By.XPATH, "//*[text() = 'allumage avant :']")
-        sh_start_time = sh_start_label_span.find_element(By.XPATH, "../../../*[3]/*[1]/*[1]").text
-        sh_end_time = sh_start_label_span.find_element(By.XPATH, "../../../*[3]/*[2]/*[1]").text
-        par = reverse_script(sh_start_label_span.find_element(By.XPATH, "../../../../../../../*[3]/*[1]/*[2]").text)
+
+        # using find_elements and not find_element as it doesn't throw if the element doesn't exist
+        sh_start_time_array = sh_start_label_span.find_elements(By.XPATH, "../../../*[3]/*[1]/*[1]")
+        sh_end_time_array = sh_start_label_span.find_elements(By.XPATH, "../../../*[3]/*[2]/*[1]")
+        par_array = sh_start_label_span.find_elements(By.XPATH, "../../../../../../../*[3]/*[1]/*[2]")
+        sh_data_found = len(sh_start_time_array) == 1 and len(sh_end_time_array) == 1 and len(par_array) == 1
+        sh_start_time = sh_start_time_array[0].text if sh_data_found else ""
+        sh_end_time = sh_end_time_array[0].text if sh_data_found else ""
+        par = reverse_script(par_array[0].text) if sh_data_found else ""
         
         result = {"tef": tef_time, "tzet": tzet_time, "sh_start": sh_start_time, "sh_end": sh_end_time, "par": par}
     except:
